840d22db2fb5757eaf7f17dba557cfaf7a876da6
ALI <alinuhov00@gmail.com>
2026-05-11 18:42:56 +0300

    render setup

--- backend/app.py
+++ backend/app.py
@@ -106,7 +106,8 @@ def create_app() -> Flask:
     return app
 
 
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
